b66c9dd8aae7030e56cf77f2702d7b2c1524848f
UlfMoberg <UlfMoberg@LAPTOP-D2OH6VRO>
2018-04-18 21:48:50 +0200

    Run Class for the Cucumber.feature

---
+++ src/projektArbete/RunStoreTests.java
@@ -0,0 +1,15 @@
+package projektArbete;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty" ,
+        "html:Reports",
+		"junit:Reports/JunitReport.xml",
+		"json:Reports/JsonReport.json"}) 
+
+public class RunStoreTests {
+}
